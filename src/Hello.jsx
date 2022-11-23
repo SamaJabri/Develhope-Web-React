9ee56791f547cd94bc78a7d49cb08491fc6d442e
Sama Jabri <sama.jabri@outlook.com>
2022-11-23 19:43:47 +0300

    Solved eighth exercise

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 
 export default class Hello extends Component {
   render() {
-    return <h1>Hello, World!</h1>;
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+        <p>What a beautiful day!</p>
+      </div>
+    );
   }
 }
