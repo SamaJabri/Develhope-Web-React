91e68555b5eb087f2271162285277821b80c203b
Sama Jabri <sama.jabri@outlook.com>
2022-11-25 17:34:56 +0300

    Solved tenth exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-import Hello from "./components/Hello";
+import Welcome from "./components/Welcome";
 
 export default class App extends Component {
   render() {
-    return (
-      <div>
-        <Hello />
-      </div>
-    );
+    return <Welcome name="Alvaro" />;
   }
 }
