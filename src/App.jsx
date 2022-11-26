f341f235f5e4e41946e963a68bf2c8976a308311
Sama Jabri <sama.jabri@outlook.com>
2022-11-26 23:12:36 +0300

    Solved twelfth exercise

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,6 @@ import Welcome from "./components/Welcome";
 
 export default class App extends Component {
   render() {
-    return <Welcome name="Alvaro" age="18" />;
+    return <Welcome name = "Alvaro" age = {18} />;
   }
 }
