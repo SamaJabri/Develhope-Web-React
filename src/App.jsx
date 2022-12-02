655755d433bfb7c9daa39460600ee961cdfa9892
Sama Jabri <sama.jabri@outlook.com>
2022-12-02 15:02:26 +0300

    Solved forty-fifth exercise

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,6 @@ import Sum from "./components/Sum";
 
 export default class App extends Component {
   render() {
-    return <Sum numbers={[1, 2, 3, 4, 5]} />;
+    return <Sum />;
   }
 }
--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Sum(props) {
-  return <h1>{props.numbers.reduce((a, b) => a + b)}</h1>;
-}
+export default function Sum({numbers = [1, 2, 3]}) {
+  return <h1>{numbers.reduce((a, b) => a + b)}</h1>;
+};
