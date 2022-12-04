b45f69f91f084dabaa49152523e6bdae6905bc89
Sama Jabri <sama.jabri@outlook.com>
2022-12-04 21:26:11 +0300

    Solved sixty-sixth exercise

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ export default class App extends Component {
             }
           />
           <Route path="users" element={<GithubUserList />}>
+            <Route index element={<p>Add a user and select it</p>} />
             <Route path=":username" element={<ShowGithubUser />} />
           </Route>
         </Routes>
