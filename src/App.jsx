fb4e83d7bb8862d53cd12da5985d97e8f5e4e3d0
Sama Jabri <sama.jabri@outlook.com>
2022-12-04 16:03:46 +0300

    Solved sixty-first exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import { Routes, Route } from "react-router-dom";
 import Welcome from "./components/welcome/Welcome";
+import Counter from "./components/counter/Counter";
 
 export default class App extends Component {
   render() {
     return (
       <Routes>
         <Route path="/" element={<Welcome name="Alvaro" />} />
+        <Route
+          path="/counter"
+          element={
+            <Counter intialValue={0} incrementBy={1} incrementInterval={1000} />
+          }
+        />
       </Routes>
     );
   }
