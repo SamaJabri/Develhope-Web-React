6e80a3fcdbcd40794ce393f127f71bea1de36bdb
Sama Jabri <sama.jabri@outlook.com>
2022-12-04 16:30:47 +0300

    Solved sixty-third exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 import React, { Component } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Welcome from "./components/welcome/Welcome";
 import Counter from "./components/counter/Counter";
+import GithubUSer from "./components/github/GithubUser";
 import ShowGithubUser from "./components/github/ShowGithubUser";
 
 export default class App extends Component {
   render() {
     return (
-      <Routes>
-        <Route path="/" element={<Welcome name="Alvaro" />} />
-        <Route
-          path="counter"
-          element={
-            <Counter intialValue={0} incrementBy={1} incrementInterval={1000} />
-          }
-        />
-        <Route path="users/:username" element={<ShowGithubUser />} />
-      </Routes>
+      <div>
+        <div>
+          <Link to="/">Home</Link> | {" "}
+          <Link to="/counter">Counter</Link> | {" "}
+          <Link to="/users/samajabri">Github Account</Link>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Welcome name="Sama" />} />
+          <Route
+            path="counter"
+            element={
+              <Counter
+                intialValue={0}
+                incrementBy={1}
+                incrementInterval={1000}
+              />
+            }
+          />
+          <Route path="users/:username" element={<ShowGithubUser />} />
+        </Routes>
+      </div>
     );
   }
 }
