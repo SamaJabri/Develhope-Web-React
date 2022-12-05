a82d0e4b3be8d8f0f89dcac517f8bbbfdaba7682
Sama Jabri <sama.jabri@outlook.com>
2022-12-05 12:18:54 +0300

    Solved redux 04 exercise

--- src/components/store.js
+++ src/components/store.js
@@ -1,10 +1,20 @@
 import { counterReducer, incrementCounter } from "./counter/CounterReducer";
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
+import { addTodo, todoReducer, editTodo } from "./todo/TodosReducer";
 
-const store = createStore(counterReducer);
+const rootReducer = combineReducers({
+    todos: todoReducer,
+    counter: counterReducer
+});
+
+const store = createStore(rootReducer);
 
 store.subscribe(() => console.log(store.getState()));
 
 store.dispatch(incrementCounter(4));
+store.dispatch(addTodo({ id: 1, title: 'Study Redux', completed: false }));
+store.dispatch(addTodo({ id: 2, title: 'DBMS Project', completed: false }));
+store.dispatch(addTodo({ id: 3, title: 'DH exercises', completed: true }));
+store.dispatch(editTodo(1, {completed: true}));
 
 export { store };
